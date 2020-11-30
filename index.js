76346950b5ab6e96ddb1411863793f4bbbadf0bd
kanu-e <chukwuemekakanu@gmail.com>
2020-11-30 16:04:48 -0600

    commit tree ef922be4dc1b905ec92bde794303075c5ba9bd74

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+   let x = record.find(s => {return s.result === "W"})
+  return x ? x.year : undefined
+}
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+
+
+
+console.log(superbowlWin(record))
